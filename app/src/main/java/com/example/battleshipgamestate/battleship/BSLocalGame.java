e681af2e38c3b7027e29acd18be1db6ab8173591
magliana22 <magliana22@up.edu>
2019-10-31 16:00:26 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/battleshipgamereal/battleship/BSLocation.java
    #	app/src/main/java/com/example/battleshipgamereal/battleship/BSShip.java
    #	app/src/main/java/com/example/battleshipgamereal/game/GameFramework/GameHumanPlayer.java
    #	app/src/main/java/com/example/battleshipgamereal/game/GameFramework/GameMainActivity.java
    #	app/src/main/java/com/example/battleshipgamestate/battleship/BSMainActivity.java
    #	app/src/main/java/com/example/battleshipgamestate/battleship/BSSurfaceView.java

--- app/src/main/java/com/example/battleshipgamestate/battleship/BSLocalGame.java
+++ app/src/main/java/com/example/battleshipgamestate/battleship/BSLocalGame.java
@@ -1,4 +1,31 @@
 package com.example.battleshipgamestate.battleship;
 
-public class BSLocalGame {
+import com.example.battleshipgamestate.game.GameFramework.LocalGame;
+
+public class BSLocalGame extends LocalGame {
+    //Tag for logging
+    private static final String TAG= "BSLocalGame";
+    //the game's state
+    protected BSState state;
+
+    /**
+     * Constructor for the BSLocalGame
+     */
+    public BSLocalGame(){
+        //use super initialization
+        super();
+
+        //create blank BSState object
+        state= new BSState();
+    }
+
+    /**
+     * check if game is over. If it is over return a String that tells
+     * who the winner is. If game not over return null
+     */
+    @Override
+    protected String checkIfGameOver(){
+
+    }
+
 }
