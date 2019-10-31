ab0bd71f1ce1e0ec75e7442d0d515b2f1a7607c7
Kyle Sanchez <sancheky22@up.edu>
2019-10-31 16:00:01 -0700

    Started working on BSComputerPlayer 1 Class

--- app/src/main/java/com/example/battleshipgamestate/battleship/BSComputerPlayer1.java
+++ app/src/main/java/com/example/battleshipgamestate/battleship/BSComputerPlayer1.java
@@ -1,4 +1,50 @@
 package com.example.battleshipgamestate.battleship;
 
-public class BSComputerPlayer1 {
+import com.example.battleshipgamestate.game.GameFramework.GameComputerPlayer;
+import com.example.battleshipgamestate.game.GameFramework.infoMessage.GameInfo;
+import com.example.battleshipgamestate.game.GameFramework.infoMessage.NotYourTurnInfo;
+import com.example.battleshipgamestate.game.GameFramework.utilities.Logger;
+
+public class BSComputerPlayer1 extends GameComputerPlayer{
+
+    /*
+     * Constructor for the BSComputerPlayer1 class
+     */
+
+    public BSComputerPlayer1(String name){
+        // invoke superclass constructor
+        super(name);
+        // invoke superclass constructor
+    }
+
+    /**
+     * Called when the player receives a game-state (or other info) from the
+     * game.
+     *
+     * @param info
+     * 		the message from the game
+     */
+    @Override
+    protected void receiveInfo(GameInfo info) {
+        if (info instanceof NotYourTurnInfo) return;
+        Logger.log("BSComputer","My turn!");
+
+        int xVal = (int)(10*Math.random());
+        int yVal = (int)(10*Math.random());
+
+
+        // delay for 2 seconds to mimic thinking
+        sleep(2);
+
+
+        // Commented out b/c LocalGame incomplete
+        //game.sendAction(new BSMoveAction(this, xVal,yVal));
+    }
+
+
+
+
+
+
+
 }
