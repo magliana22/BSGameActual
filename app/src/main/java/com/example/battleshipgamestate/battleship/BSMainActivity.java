bf3c78af32bd3b90670758e4a0653785e3ea3dfe
co22 <co22@up.edu>
2019-11-03 16:40:29 -0800

    added methods from game main activity to BS main activity.

--- app/src/main/java/com/example/battleshipgamestate/battleship/BSMainActivity.java
+++ app/src/main/java/com/example/battleshipgamestate/battleship/BSMainActivity.java
@@ -1,7 +1,18 @@
 package com.example.battleshipgamestate.battleship;
 
 import com.example.battleshipgamestate.game.GameFramework.GameMainActivity;
+import com.example.battleshipgamestate.game.GameFramework.LocalGame;
+import com.example.battleshipgamestate.game.GameFramework.gameConfiguration.GameConfig;
 
 public class BSMainActivity extends GameMainActivity {
 
+    @Override
+    public GameConfig createDefaultConfig() {
+        return null;
+    }
+
+    @Override
+    public LocalGame createLocalGame() {
+        return null;
+    }
 }
