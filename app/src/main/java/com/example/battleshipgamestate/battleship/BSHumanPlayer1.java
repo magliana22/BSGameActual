736859e7420478f84ccdf77e52f23d9ec6c587e5
Kyle Sanchez <sancheky22@up.edu>
2019-11-03 17:13:01 -0800

    MoveAction completed and ComputerPlayer completed

--- app/src/main/java/com/example/battleshipgamestate/battleship/BSHumanPlayer1.java
+++ app/src/main/java/com/example/battleshipgamestate/battleship/BSHumanPlayer1.java
@@ -1,4 +1,11 @@
 package com.example.battleshipgamestate.battleship;
 
+import android.view.View;
+
+import com.example.battleshipgamestate.game.GameFramework.GameHumanPlayer;
+import com.example.battleshipgamestate.game.GameFramework.GameMainActivity;
+import com.example.battleshipgamestate.game.GameFramework.infoMessage.GameInfo;
+
 public class BSHumanPlayer1 {
+
 }
--- app/src/main/java/com/example/battleshipgamestate/battleship/BSMoveAction.java
+++ app/src/main/java/com/example/battleshipgamestate/battleship/BSMoveAction.java
@@ -1,4 +1,41 @@
 package com.example.battleshipgamestate.battleship;
 
-public class BSMoveAction {
+import com.example.battleshipgamestate.game.GameFramework.GamePlayer;
+import com.example.battleshipgamestate.game.GameFramework.actionMessage.GameAction;
+
+public class BSMoveAction extends GameAction {
+
+
+    //Tag for logging
+    // Not sure if needed
+    private static final String TAG = "TTTMoveAction";
+    private static final long serialVersionUID = -2242980258970485343L;
+
+    // instance variables: the selected row and column
+    private int row;
+    private int col;
+
+
+    /**
+     * Constructor for BSMoveAction
+     *
+     * @param player the player who created the action
+     * @param row the row of the square selected (0-9)
+     * @param col the column of the square selected
+     */
+    public BSMoveAction(GamePlayer player, int row, int col) {
+        super(player);
+
+        this.row = Math.max(0, Math.min(9,row));
+        this.col = Math.max(0, Math.min(9,col));
+
+    }
+
+    public int getRow(){
+        return row;
+    }
+
+    public int getCol(){
+        return col;
+    }
 }
