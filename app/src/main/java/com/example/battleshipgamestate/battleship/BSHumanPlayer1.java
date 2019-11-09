012f009bff3cfb0757d9f96109e049d8443970c9
Kyle Sanchez <sancheky22@up.edu>
2019-11-09 12:04:43 -0800

    Blank version of Human Player

--- app/src/main/java/com/example/battleshipgamestate/battleship/BSHumanPlayer1.java
+++ app/src/main/java/com/example/battleshipgamestate/battleship/BSHumanPlayer1.java
@@ -11,54 +11,6 @@ import com.example.battleshipgamestate.game.GameFramework.infoMessage.IllegalMov
 import com.example.battleshipgamestate.game.GameFramework.infoMessage.NotYourTurnInfo;
 import com.example.battleshipgamestate.game.GameFramework.utilities.Logger;
 
-public class BSHumanPlayer1 implements View.OnTouchListener{
-    // the current activity
-    private Activity myActivity;
-
-    // the surface view
-    private BSSurfaceView surfaceView;
-
-    // the ID for the layout to use
-    private int layoutId;
-
-    public void receiveInfo(GameInfo info){
-        if (surfaceView == null) return;
-
-        if (info instanceof IllegalMoveInfo || info instanceof NotYourTurnInfo) {
-            // if the move was out of turn or otherwise illegal, flash the screen
-            //surfaceView.flash(Color.RED, 50);
-        }
-        else if (!(info instanceof BSState))
-            // if we do not have a BSState, ignore
-            return;
-        else {
-            //surfaceView.setState((BSState)info);
-            surfaceView.invalidate();
-    }
-    public boolean onTouch(View v, MotionEvent event){
-        // ignore if not an "up" event
-        if (event.getAction() != MotionEvent.ACTION_UP) return true;
-        // get the x and y coordinates of the touch-location;
-        // convert them to square coordinates (where both
-        // values are in the range 0..2)
-        int x = (int) event.getX();
-        int y = (int) event.getY();
-        Point p = surfaceView.mapPixelToSquare(x, y);
-
-        // if the location did not map to a legal square, flash
-        // the screen; otherwise, create and send an action to
-        // the game
-        if (p == null) {
-            //surfaceView.flash(Color.RED, 50);
-        } else {
-            BSMoveAction action = new BSMoveAction(this, p.y, p.x);
-            Logger.log("onTouch", "Human player sending TTTMA ...");
-            game.sendAction(action);
-            surfaceView.invalidate();
-        }
-
-        // register that we have handled the event
-        return true;
-    }
-
+public class BSHumanPlayer1 {
+   
 }
