3a05fed0ae24a350f26a0902b06890413dd86f16
co22 <co22@up.edu>
2019-11-18 19:04:04 -0800

    added return true to ontouch

--- app/src/main/java/com/example/battleshipgamestate/battleship/BSHumanPlayer1.java
+++ app/src/main/java/com/example/battleshipgamestate/battleship/BSHumanPlayer1.java
@@ -126,7 +126,7 @@ public class BSHumanPlayer1 extends GameHumanPlayer implements View.OnTouchListe
      */
     public boolean onTouch(View v, MotionEvent event) {
         // ignore if not an "up" event
-        if (event.getAction() != MotionEvent.ACTION_UP) {
+        if (event.getAction() != MotionEvent.ACTION_UP) return true;
             // get the x and y coordinates of the touch-location;
             // convert them to square coordinates (where both
             // values are in the range 0..9)
@@ -177,7 +177,6 @@ public class BSHumanPlayer1 extends GameHumanPlayer implements View.OnTouchListe
 
                 }
             }
-        }
         // register that we have handled the event
         return true;
 
